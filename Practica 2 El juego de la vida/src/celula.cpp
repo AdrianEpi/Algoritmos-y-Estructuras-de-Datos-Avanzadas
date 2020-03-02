24092fa1e15ebf49d7542eca5b1bd3c1dcefdbf7
Adrian Epifanio <alu0101158280@ull.edu.es>
2020-03-02 08:58:59 +0000

    vacio

---
+++ Practica 2 El juego de la vida/src/celula.cpp
@@ -0,0 +1,6 @@
+/*
+* @Author: Adrián Epifanio
+* @Date:   2020-03-02 08:56:45
+* @Last Modified by:   Adrián Epifanio
+* @Last Modified time: 2020-03-02 08:57:05
+*/
---
+++ Practica 2 El juego de la vida/src/tablero.cpp
@@ -0,0 +1,6 @@
+/*
+* @Author: Adrián Epifanio
+* @Date:   2020-03-02 08:56:36
+* @Last Modified by:   Adrián Epifanio
+* @Last Modified time: 2020-03-02 08:57:06
+*/
